02da842456a28b7a62adc356c68be2ac414886a0
Abdulloh Ahmadaliyev <abdulloh.dev.843@gmail.com>
2024-06-08 13:41:07 +0500

    tugadi

--- index.js
+++ index.js
@@ -6,11 +6,32 @@ button.addEventListener("click", (event) => {
     event.preventDefault();
 
     let vazifa = input.value;
-    let li = document.createElement("li")
+    let li = document.createElement("li");
+    let span = document.createElement("span")
 
-    li.textContent = vazifa
+    let done = document.createElement("button");
+    let del = document.createElement("button");
+   
+    done.addEventListener("click", (event) => {
+
+    })
+
+    del.addEventListener("click", (event) => {
+        event.preventDefault();
+
+    })
+
+
+
+    
+    span.textContent = vazifa;
+    done.textContent ="done";
+    del.textContent = "delete";
+
+    li.appendChild(span);
+    li.appendChild(done);
+    li.appendChild(del);
 
     list.appendChild(li);
-
-    li.value =" ";
+    input.value = " ";
 });
